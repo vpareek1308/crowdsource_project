204b903a30fe6e12e7b00d19ccf4a87f99365e29
Vivek Pareek <vpareek1308@gmail.com>
2021-07-07 12:40:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# crowdsource_project
+ A Crowdsourcing Project for study of wildlife in IITJ Campus.
---
+++ android/app/src/main/kotlin/com/example/croudsource_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.croudsource_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
